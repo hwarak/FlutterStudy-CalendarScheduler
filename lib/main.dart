e8d7380cb5d3eda198af38e83b815bd551fa4b7f
Minhee Seo <hwarakhada@gmail.com>
2022-05-25 16:38:04 +0900

    initial setting

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:calendar_schedular/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData(fontFamily: 'NotoSanse'),
+      home: HomeScreen(),
+    ),
+  );
+}
