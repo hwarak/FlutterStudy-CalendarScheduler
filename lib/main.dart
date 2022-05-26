9140ac5b3d961f056f5dcb2538a29a0378cde433
Minhee Seo <hwarakhada@gmail.com>
2022-05-26 14:21:42 +0900

    intl 패키지 사용해서 번역

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:calendar_schedular/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
-void main() {
+void main() async {
+  // 초기화가 되었는지 확이하라.
+  // 플러터 프레임워크가 준비 될 때까지 기다릴 수 있다
+  WidgetsFlutterBinding.ensureInitialized();
+  // intl패키지 안에 있는 모든 언어를 사용할 수 있게 됨 (특히 날짜와 관련된것들)
+  await initializeDateFormatting();
   runApp(
     MaterialApp(
       theme: ThemeData(fontFamily: 'NotoSanse'),
