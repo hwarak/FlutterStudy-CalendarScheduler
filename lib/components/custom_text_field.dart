03b829fd4b14c5a6563409875176cca70a9f8e57
Minhee Seo <hwarakhada@gmail.com>
2022-05-31 09:56:46 +0900

    colorPicker 생성 후 row가 아닌 wrap으로 감싸기

--- lib/components/custom_text_field.dart
+++ lib/components/custom_text_field.dart
@@ -1,17 +1,31 @@
+import 'package:calendar_schedular/constant/colors.dart';
 import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({Key? key}) : super(key: key);
+  final String label;
+  const CustomTextField({Key? key, required this.label}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      cursorColor: Colors.grey,
-      decoration: InputDecoration(
-        border: InputBorder.none, // 텍스트필드 밑에 있는 밑줄 지우기
-        filled: true, // 이걸 바꿔줘야지만 색을 변경할 수 있다
-        fillColor: Colors.grey[300],
-      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: TextStyle(
+            color: PRIMARY_COLOR,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        TextField(
+          cursorColor: Colors.grey,
+          decoration: InputDecoration(
+            border: InputBorder.none, // 텍스트필드 밑에 있는 밑줄 지우기
+            filled: true, // 이걸 바꿔줘야지만 색을 변경할 수 있다
+            fillColor: Colors.grey[300],
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/components/schedule_bottom_sheet.dart
+++ lib/components/schedule_bottom_sheet.dart
@@ -15,26 +15,96 @@ class ScheduleBottom extends StatelessWidget {
       height: MediaQuery.of(context).size.height / 2 + bottomInset,
       child: Padding(
         padding: EdgeInsets.only(bottom: bottomInset),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Expanded(
-                  child: CustomTextField(),
-                ),
-                SizedBox(
-                  width: 16.0,
-                ),
-                Expanded(child: CustomTextField()),
-              ],
-            ),
-            SizedBox(
-              height: 16.0,
-            ),
-            CustomTextField(),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 8.0,
+            right: 8.0,
+            top: 16.0,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _Time(),
+              SizedBox(
+                height: 16.0,
+              ),
+              _Contents(),
+              SizedBox(
+                width: 16.0,
+              ),
+              _ColorPicker()
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class _Time extends StatelessWidget {
+  const _Time({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: CustomTextField(
+            label: '시작 시간',
+          ),
+        ),
+        SizedBox(
+          width: 16.0,
+        ),
+        Expanded(
+          child: CustomTextField(
+            label: '마감 시간',
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _Contents extends StatelessWidget {
+  const _Contents({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomTextField(
+      label: '내용',
+    );
+  }
+}
+
+class _ColorPicker extends StatelessWidget {
+  const _ColorPicker({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      spacing: 8.0,
+      runSpacing: 10.0,
+      children: [
+        renderColor(Colors.red),
+        renderColor(Colors.orange),
+        renderColor(Colors.yellow),
+        renderColor(Colors.green),
+        renderColor(Colors.blue),
+        renderColor(Colors.indigo),
+        renderColor(Colors.purple),
+      ],
+    );
+  }
+
+  Widget renderColor(Color color) {
+    return Container(
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: color,
+      ),
+      width: 32.0,
+      height: 32.0,
     );
   }
 }
