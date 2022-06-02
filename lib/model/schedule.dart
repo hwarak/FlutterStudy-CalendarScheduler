a047f4e31724cf52651adb60403ef00767e2b4e9
Minhee Seo <hwarakhada@gmail.com>
2022-06-02 16:37:16 +0900

    테이블 생성

---
+++ lib/model/schedule.dart
@@ -0,0 +1,26 @@
+import 'package:drift/drift.dart';
+
+class Schedules extends Table {
+  // 테이블 이름을 넣어주자
+
+  // PRIMARY KEY
+  IntColumn get id => integer()(); // 필드명
+
+  // 내용
+  TextColumn get content => text()();
+
+  // 일정 날짜
+  DateTimeColumn get date => dateTime()();
+
+  // 시작 시간
+  IntColumn get startTime => integer()();
+
+  // 끝 시간
+  IntColumn get endTime => integer()();
+
+  // Category Color Table ID
+  IntColumn get colorId => integer()();
+
+  // 생성 날짜
+  DateTimeColumn get createdDate => dateTime()();
+}
